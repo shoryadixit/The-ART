4aaf6c57968502f392c448d1e021db74f242dcb1
Shorya Dixit <shauryadt123@gmail.com>
2022-05-23 22:11:22 +0530

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "9d4f966e9cfa47ebab5b0e40cb924945";
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
---
+++ test/sample-test.js
@@ -0,0 +1,51 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+    const Market = await ethers.getContractFactory("NFTMarket");
+    const market = await Market.deploy();
+    await market.deployed();
+    const marketAddress = market.address;
+
+    const NFT = await ethers.getContractFactory("NFT");
+    const nft = await NFT.deploy(marketAddress);
+    await nft.deployed();
+    const nftcontractaddress = nft.address;
+
+    let listingPrice = await market.getListingPrice();
+    listingPrice = listingPrice.toString();
+
+    const auctionPrice = ethers.utils.parseUnits("100", "ether");
+
+    await nft.createToken("https://www.mytokenlocation.com");
+    await nft.createToken("https://www.mytokenlocation2.com");
+
+    await market.createMarketItem(nftcontractaddress, 1, auctionPrice, { value: listingPrice });
+    await market.createMarketItem(nftcontractaddress, 2, auctionPrice,
+      {value: listingPrice} );
+
+    const [_, buyerAddress ] = await ethers.getSigners();
+
+    await market.connect(buyerAddress).createMarketSale(nftcontractaddress, 1,
+      {value: auctionPrice});
+
+       //fetch market items
+      let items = await market.fetchMarketItems();
+
+      items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId);
+
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item;
+    }));
+
+    console.log('items: ', items);
+  })
+});
